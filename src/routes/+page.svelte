2b4305f0e2ff42c3f69f034977bccceeaada188a
Anatoly <contact@anafro.ru>
2024-12-17 13:48:07 +1000

    feat: create default tool frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Tool, VectorImage } from "@anfr/ui";
+</script>
+
+{#snippet logotype()}
+    <VectorImage viewBox="0 0 16.82 2.43">
+        <defs>
+            <style>
+                .cls-1 {
+                    fill: #bfbdb6;
+                }
+            </style>
+        </defs>
+        <path class="cls-1" d="M15.53.16c-.08-.1-.19-.16-.32-.16H.4C.07,0-.12.37.08.64l1.22,1.63c.08.1.19.16.32.16h14.81c.33,0,.51-.37.32-.64l-1.22-1.63Z"/>
+    </VectorImage>
+{/snippet}
+
+<Tool name="Nothing" {logotype} --accent-color="#BFBDB6">
+    Hello!
+</Tool>
